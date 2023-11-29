1ca14a0001e818178ec8e9e21dbc91effbdcd0d8
Tathiana Ianelli de Oliveira <oitathi@ibm.com>
2023-11-29 16:26:15 -0300

    meu primeiro commit

---
+++ src/main/java/com/example/jenkinsjava/JenkinsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsjava/JenkinsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
